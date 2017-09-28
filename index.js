a1b873b49da29f425f612f397d5acc30b0d3476e
jgallud <jgallud@gmail.com>
2017-09-28 11:40:12 +0200

    reorganizacion

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ var http = require('http'),
     fs = require('fs'),
     // NEVER use a Sync function except at start-up!
     index = fs.readFileSync(__dirname + '/index.html');
+var exp=require("express");
+var app=exp(); 
+var http=require("http").Server(app);
+//var server=http.createServer(app);
 
-// Send index.html to all requests
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
+var io = require('socket.io').listen(http);
+
+app.use(exp.static(__dirname +"/"));
+
+app.get("/",function(req, res) {
+	var contenido=fs.readFileSync("./index.html");
+    res.setHeader("Content-type","text/html");
+    res.send(contenido);
 });
-
-// Socket.io server listens to our app
-var io = require('socket.io').listen(app);
 
 // Send current time to all connected clients
 function sendTime() {
@@ -23,9 +28,9 @@ setInterval(sendTime, 10000);
 // Emit welcome message on connection
 io.on('connection', function(socket) {
     // Use socket to communicate with this particular client only, sending it it's own id
-    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+    socket.emit('welcome', { message: 'Bienvenido!', id: socket.id });
 
     socket.on('i am client', console.log);
 });
 
-app.listen(3000);
+http.listen(3000);
